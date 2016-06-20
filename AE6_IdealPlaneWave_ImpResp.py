1bf34b89bd6afeab2b0cef406b04e905895ae12f
chris <mail@chrisclock.com>
2016-06-20 10:51:01 +0200

    clean up AEs

--- AE6_IdealPlaneWave_ImpResp.py
+++ AE6_IdealPlaneWave_ImpResp.py
@@ -1,37 +1,36 @@
 # SOFiA example 6: Impulse response reconstruction on a simulated
 #                  ideal unity plane wave.
-# SOFiA Version  : R13-0306
 # Generate a full audio spectrum plane wave using I/W/G
 
 import numpy as np
 from sofia import gen, process
 import matplotlib.pyplot as plt
 
-Nwave = 5    # Wave order
-r = 1        # Array radius
-ac = 2       # Array configuration: 2-Rigid Sphere
-FS = 48000   # Sampling Frequency
-NFFT = 1024  # FFT-Bins
-AZ = 0       # Azimuth angle
-EL = np.pi / 2  # Elevation Angle
+pi = np.pi
+Nwave = 5     # Wave order
+Nrf = Nwave   # Radial filter order
+Npdc = Nwave  # Decomposition order
+r = 1         # Array radius
+ac = 2        # Array configuration: 2-Rigid Sphere
+FS = 48000    # Sampling Frequency
+NFFT = 1024   # FFT-Bins
+AZ = 0        # Azimuth angle
+EL = pi / 2   # Elevation Angle
+OmegaL = np.array([[0, pi / 2],  # Looking directions for plane wave decomposition
+                   [pi / 2, pi / 2]])
 
 Pnm, kr = gen.wgc(Nwave, r, ac, FS, NFFT, AZ, EL)
 
-# Make radial filters
-Nrf = Nwave                  # radial filter order
+# Generate modal radial filters (MF)
 dn, _ = gen.mf(Nrf, kr, ac)
 
-# Running a plane wave decomposition for different look directions
-
-Npdc = Nwave                # Decomposition order
-OmegaL = np.array([[0, np.pi / 2], [np.pi / 2, np.pi / 2]])  # Looking towards the wave and to one side
-
+# Running a plane wave decomposition (PDC) for supplied look directions
 Y = process.pdc(Npdc, OmegaL, Pnm, dn)
 
-# Reconstruct impulse responses
+# Reconstruct time domain signal (TDT)
 impulseResponses = process.tdt(Y)
 
-#%% Plot results (Impulse Responses)
+# Plot results (Impulse Responses)
 print('Plotting...')
 
 plt.subplot(1, 2, 1)
@@ -39,13 +38,11 @@ plt.plot(impulseResponses.T)
 plt.title('Impulse response')
 plt.xlabel('Samples')
 plt.ylabel('Amplitude')
-
 plt.axis([-200, NFFT, -0.2, 1.2])
 plt.grid()
 
 
 # Plot results (Spectra)
-
 spectrum = np.abs(np.fft.fft(impulseResponses).T)
 fscale = np.fft.fftfreq(impulseResponses.shape[1], 1 / FS)
 
--- AE7_SampledPlaneWave_ImpResp.py
+++ AE7_SampledPlaneWave_ImpResp.py
@@ -1,60 +1,59 @@
 # SOFiA example 7: Impulse response reconstruction on a simulated
 #                  sampled unity plane wave.
-# SOFiA Version  : R13-0306
+# Generate a full audio spectrum plane wave using S/W/G
 
 import numpy as np
 from sofia import gen, process
 import matplotlib.pyplot as plt
 
-# Generate a full audio spectrum plane wave using S/W/G
+pi = np.pi
+Nsft = 5        # Spatial fourier transform order
+Nrf = Nsft      # Radial filter order
+Npdc = Nsft     # Decomposition order
+OmegaL = np.array([[0, pi / 2],  # Looking directions for plane wave decomposition
+                   [pi / 2, pi / 2]])
+limit = 150     # Amplification limit of radial filters
 r = 0.2         # Array radius
 ac = 2          # Rigid Sphere Array
 FS = 24000      # Sampling Frequency
 NFFT = 1024     # FFT-Bins
 AZ = 0          # Azimuth angle
-EL = np.pi / 2  # Elevation angle
+EL = pi / 2  # Elevation angle
 
-quadrature_grid, _ = gen.lebedev(110)  # Lebedev grid of order 110
+# Generate Lebedev grid of order 110
+quadrature_grid, _ = gen.lebedev(110)
 
+# Retrieve FFT of plane wave sampled by grid using the Sampled Wave Generator wrapper (SWG)
 fftData, kr = gen.swg(r=r, gridData=quadrature_grid, ac=ac, FS=FS, NFFT=NFFT, AZ=AZ, EL=EL)
 
-# Spatial Fourier Transform
-Nsft = 5
+# Spatial Fourier Transform (STC)
 Pnm = process.stc(Nsft, fftData, quadrature_grid)
 
-# Make radial filters
-Nrf = Nsft      # radial filter order
-limit = 150     # Amplification Limit (Keep the result numerical stable at low frequencies)
-
+# Generate modal radial filters (MF)
 dn, _ = gen.mf(Nrf, kr, ac, a_max=limit)
 
-# Plane wave decomposition for different look directions
-Npdc = Nsft     # Decomposition order
-OmegaL = np.array([[0, np.pi / 2], [np.pi / 2, np.pi / 2]])  # Looking towards the wave and to one side
-
+# Plane wave decomposition (PDC) for supplied look directions
 Y = process.pdc(Npdc, OmegaL, Pnm, dn)
 
-# Reconstruct impulse responses
+# Reconstruct time domain signal (TDT)
 impulseResponses = process.tdt(Y)
 
 # Make IR causal (flip first & second half):
 impulseResponses = np.hstack(np.array_split(impulseResponses, 2, axis = 1)[::-1])
 
-# TODO: fix scaling (constant factor 19)
+# TODO: fix scaling (currently constant factor 19)
 impulseResponses = impulseResponses / 19
 
-# %%Plot results (Impulse Responses)
+# Plot results (Impulse Responses)
 plt.subplot(1, 2, 1)
 plt.plot(impulseResponses.T)
 plt.title('Impulse response')
 plt.xlabel('Samples')
 plt.ylabel('Amplitude')
-
 plt.axis([0, NFFT, -0.2, 1.2])
 plt.grid()
 
 # Plot results (Spectra)
-
 spectrum = np.abs(np.fft.fft(impulseResponses).T)
 fscale = np.fft.fftfreq(impulseResponses.shape[1], 1 / FS)
 
@@ -65,3 +64,5 @@ plt.xlabel('Frequency in Hz')
 plt.ylabel('Magnitude')
 plt.axis([50, FS / 2, -60, 30])
 plt.grid()
+
+plt.show()
