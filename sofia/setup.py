186c027ebb26568cc076927dea80fce6fe95d95a
chohner <mail@chohner.com>
2016-07-26 18:04:33 +0200

    update setup.py
    ver > 0.0.3
    new url
    list packages

--- sofia/setup.py
+++ sofia/setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='sofia-py',
-    version='0.0.2',
+    version='0.0.3',
     description='Python port of the SOFiA toolbox for Matlab',
-    url='https://github.com/QULab/sofia-py',
+    url='https://qulab.github.io/sofia-py/',
     author='QU Lab / Christoph Hohnerlein',
     author_email='christoph.hohnerlein@qu.tu-berlin.de',
     license='GPLv3',
+    packages=['gen', 'io', 'lebedev', 'plot', 'process', 'sph'],
     install_requires=[
         'Numpy',
         'Scipy',
