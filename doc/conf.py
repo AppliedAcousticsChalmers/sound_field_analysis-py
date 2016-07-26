208a65223cd99835d3c38076713280aefcb8efca
chohner <mail@chohner.com>
2016-07-26 18:14:42 +0200

    update doc stuff

--- doc/conf.py
+++ doc/conf.py
@@ -148,7 +148,7 @@ html_theme = 'sphinx_rtd_theme'
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
+html_theme_path = ["_themes", ]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
