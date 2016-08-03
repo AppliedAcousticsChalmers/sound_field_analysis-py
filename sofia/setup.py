591d01b45cc0397c3d45154e98c4b2a116acbc9e
chohner <mail@chohner.com>
2016-08-03 17:54:10 +0200

    update classifiers

--- sofia/setup.py
+++ sofia/setup.py
@@ -8,13 +8,24 @@ setup(
     author='QU Lab / Christoph Hohnerlein',
     author_email='christoph.hohnerlein@qu.tu-berlin.de',
     license='GPLv3',
-    packages=['gen', 'io', 'lebedev', 'plot', 'process', 'sph', 'utils'],
-    install_requires=[
-        'Numpy',
-        'Scipy'
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Multimedia :: Sound/Audio',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.5',
     ],
+    keywords='spherical microphone array analysis',
+    install_requires=[
+        'numpy',
+        'scipy'
+    ],
+    package_data={
+        'examples': ['examples'],
+    },
     extras_require={
         'plotting': ["plotly"],
         'progress_bars': ["tqdm"]
-    }
+    },
+    packages=['gen', 'io', 'lebedev', 'plot', 'process', 'sph', 'utils']
 )
