a959e5ff1a7d607e6db617ccffd817b844628e2b
chohner <mail@chohner.com>
2016-08-09 15:20:16 +0200

    use relative import

--- sound_field_analysis/__init__.py
+++ sound_field_analysis/__init__.py
@@ -1,4 +1,4 @@
 """Analyze, visualize and process sound field data recorded by spherical microphone arrays.
 """
-from sound_field_analysis._version import __version__
+from ._version import __version__
 __all__ = ["io", "gen", "process", "plot", "sph", "utils", "lebedev"]
