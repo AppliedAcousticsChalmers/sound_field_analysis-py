9fd5f53c71f9b086621ea86f721e724126aeb277
JensAhrens <jens.ahrens@chalmers.se>
2018-11-16 16:38:30 +0100

    Fix file loading in examples

--- examples/miro_to_struct.m
+++ examples/miro_to_struct.m
@@ -58,3 +58,32 @@ avgAirTemp = miro_data.avgAirTemp;
 clear miro_data path_name file_name;
 
 save( 'data/CR1_VSA_110RS_L_struct.mat', '-v7' );
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear;
+
+file_name = 'SBS_VSA_110RS_PAC';
+path_name = 'data/';
+
+miro_data = load( [ path_name file_name ] );
+miro_data = miro_data.(file_name);
+
+name       = miro_data.name;
+context    = miro_data.context;
+location   = miro_data.location;
+date       = miro_data.date;
+irChOne    = miro_data.irChOne;
+irChTwo    = miro_data.irChTwo;
+fs         = miro_data.fs;
+azimuth    = miro_data.azimuth;
+colatitude = miro_data.elevation;
+radius     = miro_data.radius;
+quadWeight = miro_data.quadWeight;
+scatterer  = miro_data.scatterer;
+avgAirTemp = miro_data.avgAirTemp; 
+
+clear miro_data path_name file_name;
+
+save( 'data/SBS_VSA_110RS_PAC_struct.mat', '-v7' );
